bd0a12481d9fa620748786449b7731a0bb01c8d5
Hannah-Unyolo <unyolohannah@gmail.com>
2024-02-22 22:20:27 +0300

    added file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main(){
+    println("Hello Hannah")
+    modulus(9,7)
+    addFourNumbers(1,2,3,4)
+    println("I am Friendly")
+}
+
+fun printName(name: String){
+    println(name)
+}
+fun modulus(num1: Int, num2:Int):Int{
+    var results = num1 % num2
+    return results
+}
+fun addFourNumbers(num1: Int, num2: Int, num3: Int, num4: Int): Int{
+    var results = num1+num2+num3+num4
+    return results
+}
+fun printInteresting(name: String){
+    println(name)
+}
+
+
+
